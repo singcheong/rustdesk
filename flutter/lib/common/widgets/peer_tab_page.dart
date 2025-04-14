1ff0455fbe6e3570a6873a6d90f3f9c2d203d776
singcheong <95837196+singcheong@users.noreply.github.com>
2025-04-14 12:07:09 +0800

    Update peer_tab_page.dart

--- flutter/lib/common/widgets/peer_tab_page.dart
+++ flutter/lib/common/widgets/peer_tab_page.dart
@@ -44,15 +44,15 @@ EdgeInsets? _menuPadding() {
 class _PeerTabPageState extends State<PeerTabPage>
     with SingleTickerProviderStateMixin {
   final List<_TabEntry> entries = [
-    _TabEntry(RecentPeersView(
-      menuPadding: _menuPadding(),
-    )),
-    _TabEntry(FavoritePeersView(
-      menuPadding: _menuPadding(),
-    )),
-    _TabEntry(DiscoveredPeersView(
-      menuPadding: _menuPadding(),
-    )),
+    // _TabEntry(RecentPeersView(
+    //   menuPadding: _menuPadding(),
+    // )),
+    // _TabEntry(FavoritePeersView(
+    //   menuPadding: _menuPadding(),
+    // )),
+    // _TabEntry(DiscoveredPeersView(
+    //   menuPadding: _menuPadding(),
+    // )),
     _TabEntry(
         AddressBook(
           menuPadding: _menuPadding(),
@@ -60,12 +60,12 @@ class _PeerTabPageState extends State<PeerTabPage>
         ({dynamic hint}) => gFFI.abModel.pullAb(
             force: hint == null ? ForcePullAb.listAndCurrent : null,
             quiet: false)),
-    _TabEntry(
-      MyGroup(
-        menuPadding: _menuPadding(),
-      ),
-      ({dynamic hint}) => gFFI.groupModel.pull(force: hint == null),
-    ),
+    // _TabEntry(
+    //   MyGroup(
+    //     menuPadding: _menuPadding(),
+    //   ),
+    //   ({dynamic hint}) => gFFI.groupModel.pull(force: hint == null),
+    // ),
   ];
   RelativeRect? mobileTabContextMenuPos;
 
